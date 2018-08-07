e40d402eee44fc479d89a86bb52344a8f3391b94
arlen <arlenluman@gmail.com>
2018-08-07 19:16:23 +0700

    it edited2

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,6 +2,11 @@ class Food < ApplicationRecord
 	validates :name, :description, presence:true
 	validates :price, numericality: {greater_than_or_equal_to:100}
 	validates :name, uniqueness:true
-	validates :image_url, allow_blank:true, format: {with: %r{\.(gif|jpg|png)\Z}i,message:'must be a URL for GIF, JPG or PNG image.' 
- }
+	validates :image_url, allow_blank:true, format: {
+		with: %r{\.(gif|jpg|png)\Z}i,
+		message:'must be a URL for GIF, JPG or PNG image.' 
+	}
+	def self.by_letter(letter)
+	 	where("name Like ?", "#{letter}%").order(:name) 	
+	end
 end
