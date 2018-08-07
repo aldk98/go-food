12f254f61c090196644f71f182f3b0430c00ed40
arlen <arlenluman@gmail.com>
2018-08-07 19:48:11 +0700

    Factory girl created

---
+++ spec/factories/food.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do 
+	factory :food do
+		name "Nasi Uduk"
+		description "Betawi style steamed"
+		price 10000.0
+	end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -58,4 +58,22 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  # config.before(:suite) do
+  #   DatabaseCleaner.clean_with(:truncation)
+  #   DatabaseCleaner.strategy = :transaction
+  # end
+
+  # # start the transaction strategy as examples are run
+  # config.around(:each) do |example|
+  #   DatabaseCleaner.cleaning do
+  #     example.run
+  #   end
+  #   Faker::UniqueGenerator.clear
+  # end
+
+  # -cut-
+
+  config.include FactoryGirl::Syntax::Methods
+
+
 end
